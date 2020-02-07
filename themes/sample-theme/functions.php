131eb00b6380f7c5b058e39b142be7d2c86df194
dhrudeep <dhrudeepshah@gmail.com>
2020-02-07 14:00:03 -0500

    Integrated Foundation

--- themes/sample-theme/functions.php
+++ themes/sample-theme/functions.php
@@ -116,9 +116,33 @@ add_action( 'widgets_init', 'sample_theme_widgets_init' );
 function sample_theme_scripts() {
 	wp_enqueue_style( 'sample-theme-style', get_stylesheet_uri() );
 
+	wp_enqueue_style(
+		'sample-theme-foundation',
+		get_template_directory_uri() . '/assets/css/vendor/foundation.min.css',
+		null,
+		'6.5.1'
+	);
+
+	wp_enqueue_script(
+		'sample-theme-what-input',
+		get_template_directory_uri() . '/assets/js/vendor/what-input.js',
+		array( 'jquery' ),
+		'6.5.1',
+		true
+	);
+
+	wp_enqueue_script(
+		'sample-theme-foundation',
+		get_template_directory_uri() . '/assets/js/vendor/foundation.min.js',
+		array( 'jquery', 'sample-theme-what-input' ),
+		'6.5.1',
+		true
+	);
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
+	
 }
 add_action( 'wp_enqueue_scripts', 'sample_theme_scripts' );
 
