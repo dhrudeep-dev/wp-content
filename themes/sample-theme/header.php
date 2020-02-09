6bf921ed5e1a5a93b6c5b7090dc074d60f66a6fa
dhrudeep <dhrudeepshah@gmail.com>
2020-02-09 15:21:49 -0500

    Header navigation changes done

--- themes/sample-theme/header.php
+++ themes/sample-theme/header.php
@@ -34,37 +34,14 @@
 			</div>
 		</div>
 	</div>
-		<!-- /*top header */ -->
-		<img src="<?php echo get_template_directory_uri(); ?>./assets/images/header.png" alt="header" width="100%" height="auto" />
 		<!-- /*logo */ -->
 		<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
 			<img src="<?php echo get_template_directory_uri(); ?>./assets/images/logo.png" alt="Logo" width="150px" height="auto" />
-			</a>
+		</a>
 			
-          <!--home girl-->
-			<img src="<?php echo get_template_directory_uri(); ?>./assets/images/homeGirl.png" alt="girl" width="300px" height="auto" />
-		Hi Header
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$sample_theme_description = get_bloginfo( 'description', 'display' );
-			if ( $sample_theme_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $sample_theme_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+  
 
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'sample-theme' ); ?></button>
 			<?php
 			wp_nav_menu( array(
 				'theme_location' => 'menu-1',
