e7c9a9c70d3a658aa145edfa0f796944cedd47be
tayyaba <tayyaba.choudhary@hotmail.com>
2020-02-09 18:02:00 -0500

    front page section 1 done- need to add sec2

--- themes/sample-theme/header.php
+++ themes/sample-theme/header.php
@@ -29,7 +29,7 @@
     </div>
         <!-- /*logo */ -->
         <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-            <img src="<?php echo get_template_directory_uri(); ?>./assets/images/logo.png" alt="Logo" width="150px" height="auto" />
+            <img id="logo" src="<?php echo get_template_directory_uri(); ?>./assets/images/logo.png" alt="Logo" width="150px" height="auto" />
         </a>
             
   
